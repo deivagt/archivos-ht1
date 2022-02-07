ce53f4205e84450230dd329a1eff5a5e35aa70af
deivagt <david1alberto@gmail.com>
2022-02-07 11:26:21 -0600

    app funcionando

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// ht1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include "app.cpp"
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    app* miapp = new app();
+    miapp->menuPrincipal();
+
+
+}
+
